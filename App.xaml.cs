15542d66a5e5d7af42ed794e7de65a75a6536788
gairs <gair@interaxon.ca>
2023-12-13 04:59:14 -0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiOnPlatformBug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
